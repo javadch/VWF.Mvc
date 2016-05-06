94fd51ba39e5f7637f49c47715d2e6d5745cbc22
Javad Chamanara <chamanara@gmail.com>
2016-05-06 15:10:14 +0200

    Empty service library to be used as the default implementation of the multi tenancy API

---
+++ Vaiona.MultiTenancy.Services/TenantRegistrar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vaiona.Model.MTnt;
+using Vaiona.MultiTenancy.Api;
+
+namespace Vaiona.MultiTenancy.Services
+{
+    public class TenantRegistrar : ITenantRegistrar
+    {
+        public void Activate(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tenant> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Inactivate(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(object tenant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unregister(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Vaiona.MultiTenancy.Services/TenantResolver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Vaiona.Model.MTnt;
+using Vaiona.MultiTenancy.Api;
+
+namespace Vaiona.MultiTenancy.Services
+{
+    public class TenantResolver : ITenantResolver
+    {
+        public Tenant Resolve(Uri request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tenant Resolve(string request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tenant Resolve(HttpRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
